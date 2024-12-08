91906be5f0064c2d235c3214f7343068da58385b
Aaron Goldstein <bredmold@gmail.com>
2024-12-08 00:43:18 -0800

    Updating Mac stuff

--- osx.sh
+++ osx.sh
@@ -10,9 +10,14 @@ function jdk {
     export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
   elif [ "$1" = "11" ]; then
     export JAVA_HOME=$(/usr/libexec/java_home -v 11)
+  elif [ "$1" = "17" ]; then
+    export JAVA_HOME=$(/usr/libexec/java_home -v 17)
   else
     echo "Unknown java version: $1"
   fi
 }
 GIT_COMPLETION='/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash'
 alias ls='ls -GF'
+
+# HomeBrew
+__prepend_path "/opt/homebrew/bin"
